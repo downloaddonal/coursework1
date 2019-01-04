974d80a7dcf582f99bedf0f69e652f770ddd5019
downloaddonal <34678118+downloaddonal@users.noreply.github.com>
2019-01-04 07:14:34 +0000

    cq

--- courseworkone/courseworkone/Program.cs
+++ courseworkone/courseworkone/Program.cs
@@ -20,7 +20,6 @@ namespace courseworkone
             int[,] storeSales = new int[4, 4];
 
             // for loop to iterate over the needed details
-
             try
             {
                 for (int i = 0; i < storeNames.Length; i++)
@@ -28,15 +27,14 @@ namespace courseworkone
                     Console.Write("\nPlease enter the name of Store: ");
                     storeNames[i] = Console.ReadLine();
                     Console.Write("Please enter the quarterly sales for " + storeNames[i] + " please enter the digits in thousands i.e 12,000 as 12 :\n");
-                    //another for loop instead of copy and pasting the same code for the question 4 times
+                    // another for loop instead of copy and pasting the same code for the question 4 times
                     for (int j = 0; j < storeNames.Length; j++)
                     {
-                        Console.Write("Please enter the Q" + (j +1) + " sales for " + storeNames[i] + "\n");
+                        Console.Write("Please enter the Q" + (j + 1) + " sales for " + storeNames[i] + "\n");
                         storeSales[i, j] = Convert.ToInt32(Console.ReadLine());
                     }
                 }
             }
-
             catch (FormatException ex)
             {
                 Console.WriteLine(ex.Message + "Please enter a number");
@@ -45,31 +43,26 @@ namespace courseworkone
                 return;
             }
 
-            // now to calculate the totals for overal sales
-            // per store
-
+            // now to calculate the totals for overal sales per store
             dubTotals1 = storeSales[0, 0] + storeSales[0, 1] + storeSales[0, 2] + storeSales[0, 3];
             dubTotals2 = storeSales[1, 0] + storeSales[1, 1] + storeSales[1, 2] + storeSales[1, 3];
             dubTotals3 = storeSales[2, 0] + storeSales[2, 1] + storeSales[2, 2] + storeSales[2, 3];
             dubTotals4 = storeSales[3, 0] + storeSales[3, 1] + storeSales[3, 2] + storeSales[3, 3];
             dubgrandTotal = dubTotals1 + dubTotals2 + dubTotals3 + dubTotals4;
 
-            // totals
-            // all stores
+            // totals all stores
             dubOverallQ1 = storeSales[0, 0] + storeSales[1, 0] + storeSales[2, 0] + storeSales[3, 0];
             dubOverallQ2 = storeSales[0, 1] + storeSales[1, 1] + storeSales[2, 1] + storeSales[3, 1];
             dubOverallQ3 = storeSales[0, 2] + storeSales[1, 2] + storeSales[2, 2] + storeSales[3, 2];
             dubOverallQ4 = storeSales[0, 3] + storeSales[1, 3] + storeSales[2, 3] + storeSales[3, 3];
 
             // doing the percentage math bit
-
             dubstore1Percent = (dubTotals1 / dubgrandTotal) * 100;
             dubstore2Percent = (dubTotals2 / dubgrandTotal) * 100;
             dubstore3Percent = (dubTotals3 / dubgrandTotal) * 100;
             dubstore4Percent = (dubTotals4 / dubgrandTotal) * 100;
 
             // all variables have been set so we will now display the table
-
             Console.WriteLine("\nSales figures: " + "\t" + storeNames[0] + "\t" + storeNames[1] + "\t" + storeNames[2] + "\t" + storeNames[3] + "\t" + "Overall" + "\n");
             Console.WriteLine("Quarter one: " + "\t" + storeSales[0, 0] + "\t" + storeSales[1, 0] + "\t" + storeSales[2, 0] + "\t" + storeSales[3, 0] + "\t" + dubOverallQ1 + "\n");
             Console.WriteLine("Quarter two: " + "\t" + storeSales[0, 1] + "\t" + storeSales[1, 1] + "\t" + storeSales[2, 1] + "\t" + storeSales[3, 1] + "\t" + dubOverallQ2 + "\n");
